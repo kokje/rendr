c93aac1a2408d285b74e4bea0680cb39f3a71490
Yashashree Kokje <yashashree.k@gmail.com>
2015-09-17 12:16:43 -0700

    Rewrite code to allow for join between data frames, need to cast to different types. This is also better for code clarity.

--- YelpAnalytics/SimpleApp.scala
+++ YelpAnalytics/SimpleApp.scala
@@ -1,10 +1,22 @@
-/*** SimpleApp.scala ***/
+/***SimpleApp.scala ***/
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
+case class Restaurant (business_id: String, name: String, city: String, state: String )
+case class UserGraph (business_id : String, users : List[String])
+
 object SimpleApp {
-        def filterRestaurants(categories : Seq[String]) : Boolean = {
-                categories.contains("Restaurants")
+        def getRestaurantsFromBusinesses(business_id : String, name: String, city: String, state: String, categories: Seq[String]) : Restaurant = {
+                if (categories.contains("Restaurants")) {
+                        Restaurant(business_id, name, city, state)
+                }
+                else {
+                        Restaurant("-1", "-1", "-1","-1")
+                }
+        }
+
+        def filterNulls(restaurant : Restaurant) : Boolean = {
+                restaurant.business_id != "-1"
         }
 
         def main(args: Array[String]) {
@@ -15,20 +27,21 @@ object SimpleApp {
                 List("target/scala-2.10/simple-project_2.10-1.0.jar"))
                 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
+                import sqlContext.implicits._
+                import sqlContext._
+
                 // Todo : Use schema before loading table
-                val businessDF  = sqlContext.jsonFile(businessFile)
+                val businessDF = sqlContext.jsonFile(businessFile).select("business_id","name","city","state","categories")
 
-                // Dataframes are similar to rdbms tables so the filter function works on columns
-                // Converted to RDD to allow custom row filtering.
-                // Todo : Alternatively, try using foreach and create to generate a new DF. This is
-                // necessary because objects are immutable and you can't simply delete rows
-                val businessRDD = businessDF.select("business_id","categories","name","city","state").rdd
-                val businessResult = businessRDD.filter(line => filterRestaurants(line.getSeq(1)))
-                businessResult.collect().foreach(println)
+                // Todo : Assign signs to reviews > 3 positive or negative
+                val reviewsDF = sqlContext.jsonFile(reviewFile).select("business_id", "user_id")
 
-                val reviewsDF = sqlContext.jsonFile(reviewFile)
-                val reviewsRDD = reviewsDF.select("business_id", "user_id").rdd
-                val reviewsResult = reviewsRDD.map(line => (line.get(0), line.get(1))).groupByKey()
-                reviewsResult.collect().foreach(println)
+                // Todo : Use other metadata or restaurants? Type of cuisine etc? For cooler analytics?
+                val restaurantRDD = businessDF.map(p => getRestaurantsFromBusinesses(p.getString(0), p.getString(1), p.getString(2), p.getString(3), p.getSeq(4))).filter(value => filterNulls(value))
+                val joinedResult = reviewsDF.join(restaurantRDD.toDF(), "business_id")
+
+                // Todo: Use group by or not for graphx?
+                joinedResult.show()
         }
 }
+                                                                                                                                                                                      1,1           All
