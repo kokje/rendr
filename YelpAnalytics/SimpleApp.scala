2be640616744b99d141c127e808c8d70a8834b01
Yashashree Kokje <yashashree.k@gmail.com>
2015-09-22 14:11:33 -0700

    Connect to cassandra and write out data from the join of reviews and restaurants

--- YelpAnalytics/SimpleApp.scala
+++ YelpAnalytics/SimpleApp.scala
@@ -3,7 +3,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
 case class Restaurant (business_id: String, name: String, city: String, state: String )
-case class UserGraph (business_id : String, users : List[String])
+case class UserGraph (business_id : String, user_id : String, name : String, city : String, state : String)
 
 object SimpleApp {
         def getRestaurantsFromBusinesses(business_id : String, name: String, city: String, state: String, categories: Seq[String]) : Restaurant = {
@@ -23,8 +23,9 @@ object SimpleApp {
                 val reviewFile = "../reviews.json" // Dump of yelp reviews
                 val businessFile = "../businesses.json" // Dump of yelp business metadata
 
-                val sc = new SparkContext("local", "Simple App", "/usr/local/spark",
-                List("target/scala-2.10/simple-project_2.10-1.0.jar"))
+                val conf = new SparkConf().setAppName("Simple App").setMaster("local").setJars(List("target/scala-2.10/simple-project_2.10-1.0.jar")).set("spark.cassandra.connection.host", "172.31.19.62")
+                val sc = new SparkContext(conf)
+
                 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
                 import sqlContext.implicits._
@@ -33,15 +34,16 @@ object SimpleApp {
                 // Todo : Use schema before loading table
                 val businessDF = sqlContext.jsonFile(businessFile).select("business_id","name","city","state","categories")
 
-                // Todo : Assign signs to reviews > 3 positive or negative
+                // Todo : Review signs > 3 positive or negative
                 val reviewsDF = sqlContext.jsonFile(reviewFile).select("business_id", "user_id")
 
-                // Todo : Use other metadata or restaurants? Type of cuisine etc? For cooler analytics?
                 val restaurantRDD = businessDF.map(p => getRestaurantsFromBusinesses(p.getString(0), p.getString(1), p.getString(2), p.getString(3), p.getSeq(4))).filter(value => filterNulls(value))
                 val joinedResult = reviewsDF.join(restaurantRDD.toDF(), "business_id")
 
-                // Todo: Use group by or not for graphx?
-                joinedResult.show()
+                // Should we group by at this stage or dump everything?
+                val collection = joinedResult.map(line => UserGraph(line.getString(0), line.getString(1), line.getString(2),line.getString(3), line.getString(4)))
+                // Does this override or append?
+                collection.saveToCassandra("test", "cities", SomeColumns("business_id", "user_id", "name","city", "state"))
         }
 }
-                                                                                                                                                                                      1,1           All
+     
