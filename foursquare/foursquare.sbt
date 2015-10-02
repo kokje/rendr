9c248064e175861c2e28533125ce3bd3726a07f9
Yashashree Kokje <yashashree.k@gmail.com>
2015-10-02 04:24:27 +0000

    Parse foursquare raw data

---
+++ foursquare/foursquare.sbt
@@ -0,0 +1,11 @@
+name := "FoursquareProject"
+
+version := "1.0"
+
+scalaVersion := "2.10.5"
+
+libraryDependencies ++= Seq(
+"org.apache.spark" %% "spark-core" % "1.4.1",
+"org.apache.spark" %% "spark-mllib" % "1.4.1"
+)
+
---
+++ foursquare/src/main/scala/FoursquareDataSet.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+
+case class Rating (user_id : Int, venue_id : Int, rating : Int)
+case class Venue  (venue_id : Int, lat : Double, long : Double)
+case class Business  (business_id : String, lat : Double, long : Double)
+
+object FoursquareDataSet {
+	
+	def main(args : Array[String]) {
+		val ratingsFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/foursquare/reviews_small.dat" // Dump of foursquare reviews
+                val venuesFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/foursquare/venues_small.dat" // Dump of foursquare business metadata
+		val businessesFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/yelp/businesses_small.json" // Dump of yelp business metadata
+                
+		val sc = new SparkContext("local", "FoursquareDataSet", "/usr/local/spark",List("target/scala-2.10/foursquareproject_2.10-1.0.jar"))
+		
+		//val conf = new SparkConf().setAppName("FoursquareDataSet").set("spark.executor.memory", "2g").set("spark.eventLog.enabled", "true").set("spark.eventLog.dir", "/mnt/data/sparklog")
+		//val sc = new SparkContext(conf)
+		val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+		import sqlContext.implicits._
+                import sqlContext._
+
+		val ratings = sc.textFile(ratingsFile).map { line =>
+      			val fields = line.split("\\s+")
+       			Rating(fields(1).toInt, fields(3).toInt, fields(5).toInt)
+    		}.cache()
+		//ratings.collect().foreach(println)
+		
+		val venues = sc.textFile(venuesFile).map { line =>
+                        val fields = line.split("\\s+")
+                        Venue(fields(1).toInt, fields(3).toDouble, fields(5).toDouble)
+                }.cache()
+		venues.collect().foreach(println)
+	}
+}
