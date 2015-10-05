9d194a44081b1a590e184263b0deb66b72c6ece1
Yashashree Kokje <yashashree.k@gmail.com>
2015-10-05 01:01:25 +0000

    Clean out all the foursquare data and write back to hdfs

--- foursquare/src/main/scala/FoursquareDataSet.scala
+++ foursquare/src/main/scala/FoursquareDataSet.scala
@@ -1,23 +1,23 @@
-package rendr
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
+import rendr.{Business, GeoHash}
 
 case class Rating (user_id : Int, rating : Int)
 case class Venue  (venue_id: Int, lat: Double, long: Double)
 
-object FoursquareDataSet {
+object FoursquareProject {
 	
 	def main(args : Array[String]) {
-		val ratingsFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/foursquare/reviews_small.dat" // Dump of foursquare reviews
-                val venuesFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/foursquare/venues_small.dat" // Dump of foursquare business metadata
-		val businessesFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/foursquare/businesses_small.json" // Dump of yelp business metadata
+		val ratingsFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/foursquare/ratings_full.dat" // Dump of foursquare reviews
+                val venuesFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/foursquare/venues_cleaned.dat" // Dump of foursquare business metadata
+		val businessesFile = "hdfs://ec2-54-193-103-208.us-west-1.compute.amazonaws.com:9000/yelp/businesses_full.json" // Dump of yelp business metadata
                 
-		val sc = new SparkContext("local", "FoursquareDataSet", "/usr/local/spark",List("target/scala-2.10/foursquareproject_2.10-1.0.jar"))
+		//val sc = new SparkContext("local", "FoursquareDataSet", "/usr/local/spark",List("target/scala-2.10/foursquareproject_2.10-1.0.jar"))
 		
-		//val conf = new SparkConf().setAppName("FoursquareDataSet").set("spark.executor.memory", "2g").set("spark.eventLog.enabled", "true").set("spark.eventLog.dir", "/mnt/data/sparklog")
-		//val sc = new SparkContext(conf)
+		val conf = new SparkConf().setAppName("FoursquareProject").set("spark.executor.memory", "2g")//.set("spark.eventLog.enabled", "true").set("spark.eventLog.dir", "/mnt/data/sparklog")
+		val sc = new SparkContext(conf)
 		val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 		import sqlContext.implicits._
                 import sqlContext._
@@ -27,7 +27,7 @@ object FoursquareDataSet {
 		val ratings = sc.textFile(ratingsFile).map { line =>
       			val fields = line.split("\\s+")
        			(fields(3).toInt,Rating(fields(1).toInt, fields(5).toInt))
-    		}.cache()
+    		}
 		//ratings.foreach(println)
 		
 		// Read fourquare venues info from file
@@ -35,7 +35,7 @@ object FoursquareDataSet {
 		val venues = sc.textFile(venuesFile).map { line =>
                         val fields = line.split("\\s+")
                         Venue(fields(1).toInt, fields(3).toDouble, fields(5).toDouble)
-                }.cache()
+                }
 		
 		// Generate geohash of venues
 		// RDD[Geohash, Venue]
@@ -54,8 +54,8 @@ object FoursquareDataSet {
 		val restaurantsYelpFoursquare = hashedVenueYelp.join(hashedVenueFoursquare).map(line => (line._2._2, line._2._1))
 		
 		// Join restaurants from foursquare with ratings
-		// RDD[VenueID, (Restaurant,Rating)] -> RestaurantID | UserID | Rating
-		restaurantsYelpFoursquare.join(ratings).map(line => (line._2._1.business_id, line._2._2.user_id, line._2._2.rating)).saveAsTextFile("foursquare_clean")	
-		 		
+		// Todo: filter out rating < 3
+		// RDD[VenueID, (Restaurant, Rating)] -> RestaurantID | UserID | Name | City | State
+		restaurantsYelpFoursquare.join(ratings).map(line => (line._2._1.business_id, line._2._2.user_id, line._2._1.name, line._2._1.city, line._2._1.state)).collect().foreach(println)	
 	}
 }
